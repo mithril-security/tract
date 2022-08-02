434f9fb704e9180a4288c5c7da2ee735aa7b8cb9
JoFrost <mebessaa@student.42.fr>
2022-08-02 14:00:15 +0200

    bump onnx proto files and fix issues with optional field

--- onnx/src/tensor.rs
+++ onnx/src/tensor.rs
@@ -105,7 +105,7 @@ fn common_tryfrom(t: &TensorProto, path: Option<&str>) -> TractResult<Tensor> {
     let dt = DataType::from_i32(t.data_type).unwrap().try_into()?;
         let shape: Vec<usize> = t.dims.iter().map(|&i| i as usize).collect();
         // detect if the tensor is rather in an external file than inside the onnx file directly
-        let is_external = t.data_location.is_some() && t.data_location == Some(1);
+        let is_external = t.data_location == 1;
         if t.raw_data.len() > 0 {
             create_tensor(shape, dt, &t.raw_data)
         } 
